dbcf97e57103361b51b54b45667409bb5cd2cb6f
SvarturAbgrund <svartur.abgrund@gmail.com>
2026-02-12 19:55:17 -0600

    Nueva Interface

---
+++ EcoWeb/public/js/script.js
@@ -0,0 +1,3 @@
+function enviarComentario() {
+    alert("Gracias por tu comentario. ¡Tu opinión es importante para EcoWeb!");
+}
--- EcoWeb/script.js
+++
@@ -1,3 +0,0 @@
-function enviarComentario() {
-    alert("Gracias por tu comentario. ¡Tu opinión es importante para EcoWeb!");
-}
